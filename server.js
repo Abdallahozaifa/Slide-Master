2cc7de02b020a50162fc582fd3bd84dce8eeaf2f
Hozaifa Abdalla <abdallahozaifa19527@gmail.com>
2016-04-16 02:49:01 +0000

    Add foundationJS responsive framework

--- server.js
+++ server.js
@@ -3,13 +3,17 @@ var express = require('express');
 var fs = require('fs');
 var app = express();
 
+/* Main Page for Slide Master */
 app.get('/SlideMaster', function(req,res){
+    /* Sends the slidemaster html page to the user */
     fs.readFile('slidemaster.html', 'utf8', function(err, data){
         if(!err) res.send(data);
         else return console.log(err);
     });
 });
 
+
+/* Listens on the cloud9 Port */
 app.listen(process.env.PORT, function() {
   console.log('App listening at http://%s:%s', process.env.IP, process.env.PORT);
 });
