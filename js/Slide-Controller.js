95d1ebb9214a4b6e3aa2ab8323c7b79c421f1a0f
Hozaifa Abdalla <abdallahozaifa19527@gmail.com>
2016-04-22 02:01:00 +0000

    Add Slide-Controller.js

---
+++ js/Slide-Controller.js
@@ -0,0 +1,32 @@
+/* Slide Controller that contains all the slide componenets and getters necessary for controller the slide master */
+var slideController = {
+     /* Slide Components */
+     noteInput: null, //The text field users can add notes via
+     addBtn: null, //Button pressed to append note input to notes
+     noteArea: null, //Area notes are displayed in
+     slideInfo: null, //The slide dislay iframe
+     lecture: null, // lecture object that contains all the slides
+     curSlideNum: 0, // current slide number that represents the current slide
+     SLIDEMIN: 0, // minimum slide number constant 
+     SLIDEMAX: 10, // maximum slide number constant
+     SLIDESHOW_ON: false, // wether the slide show is on or not
+
+     /* Grabs the div element inside the first iframe */
+     getSlideFrame: function() {
+          return $($("iframe")[0]).contents().find("#slide-page");
+     },
+     /* Grabs the note input textfield in the second iframe */
+     getNoteInput: function() {
+          return $($("iframe")[1]).contents().find("#noteContent");
+     },
+
+     /* Grabs the add button next to the input field in the second iframe */
+     getAddNoteBtn: function() {
+          return $($("iframe")[1]).contents().find("#addNote");
+     },
+
+     /* Grabs the note area that displays the notes in the third iframe */
+     getNoteArea: function() {
+          return $($("iframe")[2]).contents().find("#user-notes");
+     }
+};
--- js/slideMaster.js
+++ js/slideMaster.js
@@ -1,16 +1,5 @@
-/* Global Space */
-var noteInput; //The text field users can add notes via
-var addBtn; //Button pressed to append note input to notes
-var noteArea; //Area notes are displayed in
-var slideInfo; //The slide dislay iframe
-var lecture; // lecture object that contains all the slides
-var curSlideNum = 0; // current slide number that represents the current slide
-var SLIDEMIN = 0; // minimum slide number constant 
-var SLIDEMAX = 10; // maximum slide number constant
-var SLIDESHOW_ON = false; // wether the slide show is on or not
-
 $(document).ready(function() {
-    
+
     var debugModeOn = true;
     /* Prints the string out to the console depending on debug mode */
     var debugOut = function(str) {
@@ -19,46 +8,24 @@ $(document).ready(function() {
         }
     };
 
-    /* Iframe Components object that contains the get methods neccessary to grab the components*/
-    var iframeComps = {
-        /* Grabs the div element inside the first iframe */
-        getSlideFrame: function() {
-            return $($("iframe")[0]).contents().find("#slide-page");
-        },
-        /* Grabs the note input textfield in the second iframe */
-        getNoteInput: function() {
-            return $($("iframe")[1]).contents().find("#noteContent");
-        },
-
-        /* Grabs the add button next to the input field in the second iframe */
-        getAddNoteBtn: function() {
-            return $($("iframe")[1]).contents().find("#addNote");
-        },
-
-        /* Grabs the note area that displays the notes in the third iframe */
-        getNoteArea: function() {
-            return $($("iframe")[2]).contents().find("#user-notes");
-        }
-    };
-
     //Gets iframe contents if any control variables are null
     var checkContents = function() {
-        if (noteInput == undefined)
-            noteInput = iframeComps.getNoteInput();
-        if (addBtn == undefined)
-            addBtn = iframeComps.getAddNoteBtn();
-        if (noteArea == undefined)
-            noteArea = iframeComps.getNoteArea();
-        if (slideInfo == undefined)
-            slideInfo = iframeComps.getSlideFrame();
+        if (slideController.noteInput == undefined)
+            slideController.noteInput = slideController.getNoteInput();
+        if (slideController.addBtn == undefined)
+            slideController.addBtn = slideController.getAddNoteBtn();
+        if (slideController.noteArea == undefined)
+            slideController.noteArea = slideController.getNoteArea();
+        if (slideController.slideInfo == undefined)
+            slideController.slideInfo = slideController.getSlideFrame();
     };
 
     /* Prints the frame contents to check it grabbed it correctly */
     var printFrameContents = function() {
-        debugOut(noteInput);
-        debugOut(addBtn);
-        debugOut(noteArea);
-        debugOut(slideInfo);
+        debugOut(slideController.noteInput);
+        debugOut(slideController.addBtn);
+        debugOut(slideController.noteArea);
+        debugOut(slideController.slideInfo);
     };
 
     /* Page header object that contains the course title, lecture title, and current slide number */
@@ -82,11 +49,22 @@ $(document).ready(function() {
 
     /* Changes the current slide */
     var changeSlide = function(command) {
-        (command == "next") && (curSlideNum < SLIDEMAX - 1) ? curSlideNum++ : null;
-        (command == "prev") && (curSlideNum > SLIDEMIN) ? curSlideNum-- : null;
-        createSlide(lecture, curSlideNum);
+        (command == "next") && (slideController.curSlideNum < slideController.SLIDEMAX - 1) ? slideController.curSlideNum++ : null;
+        (command == "prev") && (slideController.curSlideNum > slideController.SLIDEMIN) ? slideController.curSlideNum-- : null;
+        createSlide(slideController.lecture, slideController.curSlideNum);
     };
-    // checkContents();
+    
+    /* Adds the handler for the add note button */
+    var addNoteBtnHandler = function() {
+        var noteBtn = slideController.getAddNoteBtn();
+        var noteArea = slideController.getNoteArea();
+        var usrNote = slideController.getNoteInput();
+        
+        noteBtn.on("click", function() {
+            noteArea.text(usrNote.val());
+            usrNote.val('');
+        });
+    };
 
     /* Icons object that contains all the icons on the page */
     var icons = {
@@ -103,16 +81,23 @@ $(document).ready(function() {
         checkContents();
         /*Opens presentation file from /resources/xxx.json*/
         $.getJSON("resources/lecture.json", function(data) {
-            lecture = data;
+            slideController.lecture = data;
             createSlide(data, 0);
         });
-        SLIDESHOW_ON = true;
+        /* Turns ON the slide show */
+        slideController.SLIDESHOW_ON = true;
+        
+        /* Changes the background color */
+        slideController.getSlideFrame().css("background-color", "grey");
+        
+        /*Registers the Add Notes button handler */
+        addNoteBtnHandler();
     });
 
     /* Proceeds to the next slide */
     $(icons.up).click(function() {
         checkContents();
-        if(SLIDESHOW_ON){
+        if (slideController.SLIDESHOW_ON) {
             changeSlide("next");
         }
     });
@@ -120,7 +105,7 @@ $(document).ready(function() {
     /* Moves back a slide */
     $(icons.down).click(function() {
         checkContents();
-        if(SLIDESHOW_ON){
+        if (slideController.SLIDESHOW_ON) {
             changeSlide("prev");
         }
     });
@@ -128,27 +113,22 @@ $(document).ready(function() {
     /* Closes the presentation */
     $(icons.stop).click(function() {
         checkContents();
-        SLIDESHOW_ON = false;
+        slideController.SLIDESHOW_ON = false;
     });
 
 
     /* Plays audio during the presentation */
     $(icons.audio).click(function() {
         checkContents();
+        if (slideController.SLIDESHOW_ON) {
+            // trigger audio
+        }
     });
 
     /* Saves the notes */
     $(icons.save).click(function() {
-        checkContents();
+        if (slideController.SLIDESHOW_ON) {
+            // save the notes 
+        }
     });
-
-    //Event handler for the add note button
-    //Takes contents of note input area and appends to the note 
-    //data object for the current lecture
-    // addBtn.on("click", function() {
-    //     if (lecture != null) {
-    //         noteArea.val(noteInput);
-    //     }
-    //     noteArea.val('');
-    // });
 });
